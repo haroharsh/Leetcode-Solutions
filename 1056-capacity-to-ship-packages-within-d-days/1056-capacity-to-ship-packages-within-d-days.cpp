eee1b04dcc0deb6bb0be599b83d287916d4493d1
Harshit Choudhary <harshitnariyal@gmail.com>
2025-11-22 01:38:23 +0530

    Time: 17 ms (15.73%), Space: 35 MB (84.98%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -3,20 +3,20 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int min= 0;
         int tot = 0;
-        for(int weight : weights){
-            min = max(min, weight);
-            tot = tot + weight;
+        for(int i=0; i < weights.size();i++){
+            min = max(min, weights[i]);
+            tot = tot + weights[i];
         }
         while(min < tot){
             int mid = min + (tot - min)/2;
             int day = 1;
             int current = 0;
-            for(int weight : weights){
-                if(current + weight > mid){
+            for(int i=0; i <weights.size();i++){
+                if(current + weights[i] > mid){
                     day++;
                     current = 0;
                 }
-                current = current + weight;
+                current = current + weights[i];
             }
             if(day>days){
                 min = mid + 1;
